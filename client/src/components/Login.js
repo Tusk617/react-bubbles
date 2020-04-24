f511daa051b1c4319a2719299190a51d39e52add
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-04-24 11:13:24 -0400

    Created the form for login and created state for both

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [credentials, setCredentials] = useState({
+
+    username: '',
+    password: ''
+    
+  })
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form>
+        <input 
+          type="text"
+          placeholder="Username:"
+        /><br/>
+        <input 
+          type="password"
+          placeholder="password"
+        
+        />
+      </form>
     </>
   );
 };
